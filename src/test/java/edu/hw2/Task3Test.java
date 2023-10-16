a5548153904365b2fc0a772d0159708f0f85c230
Zer-01 <oko48ru@gmail.com>
2023-10-16 23:35:20 +0300

    add hw2 Task3, 4 solution and test

---
+++ src/test/java/edu/hw2/Task3Test.java
@@ -0,0 +1,36 @@
+package edu.hw2;
+
+import edu.hw2.Task3.ConnectionException;
+import edu.hw2.Task3.DefaultConnectionManager;
+import edu.hw2.Task3.FaultyConnectionManager;
+import edu.hw2.Task3.PopularCommandExecutor;
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
+
+public class Task3Test {
+    @Test
+    void normalTest() {
+        PopularCommandExecutor executor = new PopularCommandExecutor(new DefaultConnectionManager(), 1);
+        executor.updatePackages();
+
+        int activeConnect = executor.getActiveConnectionsCount();
+
+        assertThat(activeConnect)
+            .isEqualTo(0); //Исключений не было, подключение закрылось => работает
+    }
+
+    @Test
+    void catchException() {
+        PopularCommandExecutor executor = new PopularCommandExecutor(new DefaultConnectionManager(), 1);
+
+        for(int i = 0; i<2;i++) {
+            executor.updatePackages(); //первые 2 выполнения - удачные
+        }
+
+        assertThatExceptionOfType(ConnectionException.class)
+            .isThrownBy(()->{
+                executor.updatePackages();
+            });
+    }
+}

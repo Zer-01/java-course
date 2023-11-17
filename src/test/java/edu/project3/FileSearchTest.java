343bc7ddc8916c3c907db2cb9760b236345c10dc
Zer-01 <oko48ru@gmail.com>
2023-11-18 01:53:15 +0300

    fix build errors(try 1)

--- src/test/java/edu/project3/FileSearchTest.java
+++ src/test/java/edu/project3/FileSearchTest.java
@@ -13,6 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class FileSearchTest {
     static @TempDir Path tmpDir;
+    static char delim;
 
     @BeforeAll
     public static void init() throws IOException {                //  tmpDir
@@ -23,12 +24,14 @@ public class FileSearchTest {
         Files.createFile(tmpDir.resolve("testFile0.txt"));  //  |-testFile0.txt
         Files.createFile(testDir1.resolve("testFile1.txt"));//
         Files.createFile(testDir2.resolve("testFile2.txt"));//
+
+        delim = System.getProperty("os.name").contains("Windows") ? '\\' : '/';
     }
 
     @Test
     void fullPathSearchTest() {
         ArgumentContainer input =
-            new ArgumentContainer(tmpDir.toString() + "\\testDir1\\testFile1.txt", null, null, null);
+            new ArgumentContainer(resolveAndToString(tmpDir, "testDir1", "testFile1.txt"), null, null, null);
         List<Path> expResult = List.of(tmpDir.resolve("testDir1").resolve("testFile1.txt"));
 
         List<Path> result = LogsSourcesParser.parse(input);
@@ -40,7 +43,7 @@ public class FileSearchTest {
     @Test
     void notFullPathSearchTest() {
         ArgumentContainer input =
-            new ArgumentContainer(tmpDir.toString() + "\\**\\testFile2.txt", null, null, null);
+            new ArgumentContainer(resolveAndToString(tmpDir, "**", "testFile2.txt"), null, null, null);
         List<Path> expResult = List.of(tmpDir.resolve("testDir2").resolve("testFile2.txt"));
 
         List<Path> result = LogsSourcesParser.parse(input);
@@ -52,7 +55,7 @@ public class FileSearchTest {
     @Test
     void nutFullNameOfFileTest() {
         ArgumentContainer input =
-            new ArgumentContainer(tmpDir.toString() + "\\testDir2\\tes*", null, null, null);
+            new ArgumentContainer(resolveAndToString(tmpDir, "testDir2", "tes*"), null, null, null);
         List<Path> expResult = List.of(tmpDir.resolve("testDir2").resolve("testFile2.txt"));
 
         List<Path> result = LogsSourcesParser.parse(input);
@@ -64,7 +67,7 @@ public class FileSearchTest {
     @Test
     void nutFullNameAndPathOfFileTest() {
         ArgumentContainer input =
-            new ArgumentContainer(tmpDir.toString() + "\\**\\tes*", null, null, null);
+            new ArgumentContainer(resolveAndToString(tmpDir, "**", "tes*"), null, null, null);
         List<Path> expResult = List.of(
             tmpDir.resolve("testDir1").resolve("testFile1.txt"),
             tmpDir.resolve("testDir2").resolve("testFile2.txt"),
@@ -80,11 +83,20 @@ public class FileSearchTest {
     @Test
     void fileNotExistTest() {
         ArgumentContainer input =
-            new ArgumentContainer(tmpDir.toString() + "\\**\\nope", null, null, null);
+            new ArgumentContainer(resolveAndToString(tmpDir, "**", "nope"), null, null, null);
 
         List<Path> result = LogsSourcesParser.parse(input);
 
         assertThat(result)
             .isEmpty();
+    }
+
+    private String resolveAndToString(Path dir, String... strings) {
+        StringBuilder builder = new StringBuilder();
+        builder.append(dir.toString());
+        for (String str : strings) {
+            builder.append(delim).append(str);
+        }
+        return builder.toString();
     }
 }

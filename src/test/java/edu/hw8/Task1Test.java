f95753fb13732c0ac3e5484d32bc6d5864ff9f66
Zer-01 <oko48ru@gmail.com>
2023-11-28 16:40:28 +0300

    fix Task1 test

--- src/test/java/edu/hw8/Task1Test.java
+++ src/test/java/edu/hw8/Task1Test.java
@@ -4,6 +4,8 @@ import edu.hw8.Task1.Client;
 import edu.hw8.Task1.Server;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class Task1Test {
     @Test
@@ -29,16 +31,18 @@ public class Task1Test {
         Server server = new Server(4);
         Client client1 = new Client();
         Client client2 = new Client();
+        boolean[] isContinue = {true};
         String in = "личности";
         String expOut = "Не переходи на личности там, где их нет";
         server.startInNewThread();
         Thread.sleep(150);
         client1.connect();
-        Thread thread = new Thread(()->clientTester(client2));
+        Thread thread = new Thread(()->clientTester(client2, isContinue));
         thread.start();
 
         String out = client1.sendRequest(in);
-        thread.interrupt();
+        isContinue[0] = false;
+        thread.join();
         client1.close();
         client2.close();
         server.stop();
@@ -47,12 +51,22 @@ public class Task1Test {
             .isEqualTo(expOut);
     }
 
-    private void clientTester(Client client) {
+    @Test
+    void invalidMaxConnections() {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(()->{
+                new Server(-2);
+            });
+    }
+
+    private void clientTester(Client client, boolean[] isContinue) {
         client.connect();
         String in = "личности";
         String out = "Не переходи на личности там, где их нет";
-        while (client.sendRequest(in).equals(out)) {
+        while (isContinue[0]) {
+            if(!client.sendRequest(in).equals(out)) {
+                throw new RuntimeException("Wrong answer");
+            }
         }
-        throw new RuntimeException("Wrong answer");
     }
 }

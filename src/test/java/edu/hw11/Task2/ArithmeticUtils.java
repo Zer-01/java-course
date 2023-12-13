6c0fe8589c375229e663b17ca93f6409708c0508
Zer-01 <oko48ru@gmail.com>
2023-12-14 00:57:35 +0300

    fix build error(try 4)

--- src/test/java/edu/hw11/Task2/ArithmeticUtils.java
+++ src/test/java/edu/hw11/Task2/ArithmeticUtils.java
@@ -1,9 +1,6 @@
 package edu.hw11.Task2;
 
 class ArithmeticUtils {
-    private ArithmeticUtils() {
-    }
-
     public static int sum(int a, int b) {
         return a + b;
     }
--- src/test/java/edu/hw11/Task2/DelegationClass.java
+++ src/test/java/edu/hw11/Task2/DelegationClass.java
@@ -1,9 +1,6 @@
 package edu.hw11.Task2;
 
 class DelegationClass {
-    private DelegationClass() {
-    }
-
     public static int sum(int a, int b) {
         return a * b;
     }
